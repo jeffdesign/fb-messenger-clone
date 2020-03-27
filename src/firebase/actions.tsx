206d1352438d65f13242343ecba021fcadc2aaf4
Jeffrey Örnerstedt <jeffrey.kontakt@gmail.com>
2020-03-28 00:17:40 +0100

    add fb actions

---
+++ src/firebase/actions.tsx
@@ -0,0 +1,53 @@
+import firebase from "./config"
+
+export const firebaseLogin = ({
+  event,
+  form,
+}: {
+  event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  form: { email: string; password: string }
+}) => {
+  event.preventDefault()
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(form.email, form.password)
+    .then((resp) => {
+      console.log(resp)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+export const firebaseSignup = ({
+  event,
+  form,
+}: {
+  event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  form: { email: string; password: string }
+}) => {
+  event.preventDefault()
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(form.email, form.password)
+    .then((resp) => {
+      console.log(resp)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+export const firebaseLogout = () => {
+  console.log("logout")
+
+  firebase
+    .auth()
+    .signOut()
+    .then((resp) => {
+      console.log(resp)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
